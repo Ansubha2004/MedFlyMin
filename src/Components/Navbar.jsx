5d716684027356f10b7bcb48d40f9a3b620cd605
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-04-13 23:52:56 +0530

    placed logo

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="w-screen min-h-auto">
+      <img src="/logo.png" className="h-[75px] sm:mx-[25px] sm:my-[20px] sm:h-[94px]" />
+    </div>
+  )
+}
